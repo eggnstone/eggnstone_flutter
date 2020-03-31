9b4c6d03e61d6dd034305f6e88ad1e07ff615e5c
Mark Eggenstein <>
2020-03-31 20:05:33 +0200

    Added LoggerMixin.staticLogger.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -50,6 +50,6 @@
 
 * Added SharedPreferencesService.
 
-## [0.1.5] - 
+## [0.1.5] - 31.03.2020
 
-* 
+* Added LoggerMixin.staticLogger.
--- lib/services/logger/LoggerMixin.dart
+++ lib/services/logger/LoggerMixin.dart
@@ -3,6 +3,9 @@ import 'package:get_it/get_it.dart';
 
 mixin LoggerMixin
 {
+    static LoggerService get staticLogger
+    => GetIt.instance.get<LoggerService>();
+
     LoggerService get logger
     => GetIt.instance.get<LoggerService>();
 }
