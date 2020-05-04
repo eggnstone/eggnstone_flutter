0ddaf8d449675e68e1ea1001cc6a3f575316112d
Mark Eggenstein <>
2020-05-04 10:47:59 +0200

    Deprecated generic "analytics" name.

--- lib/services/analytics/AnalyticsMixin.dart
+++ lib/services/analytics/AnalyticsMixin.dart
@@ -3,9 +3,17 @@ import 'package:get_it/get_it.dart';
 
 mixin AnalyticsMixin
 {
+    @Deprecated('Use staticGoogleAnalytics instead')
     static IAnalyticsService get staticAnalytics
     => GetIt.instance.get<IAnalyticsService>();
 
+    static IAnalyticsService get staticGoogleAnalytics
+    => GetIt.instance.get<IAnalyticsService>();
+
+    @Deprecated('Use googleAnalytics instead')
     IAnalyticsService get analytics
+    => GetIt.instance.get<IAnalyticsService>();
+
+    IAnalyticsService get googleAnalytics
     => GetIt.instance.get<IAnalyticsService>();
 }
