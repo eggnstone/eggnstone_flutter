a263cb3cda4351523b10a93771235dea252aafec
Mark Eggenstein <>
2021-02-27 16:23:21 +0100

    v0.4.3: Improved API documentation.

--- lib/services/analytics/IAnalyticsService.dart
+++ lib/services/analytics/IAnalyticsService.dart
@@ -12,34 +12,27 @@ abstract class IAnalyticsService
     /// The current screen.
     set currentScreen(String newValue);
 
-    /// Track an event using a @name and optional @params.
+    /// Track an event using a [name] and optional [params].
     Future track(String name, [Map<String, dynamic>? params]);
 
-    /// Track a warning using a warning @message and optional @params.
+    /// Track a warning using a warning [message] and optional [params].
     Future trackWarning(String message, [Map<String, dynamic>? params]);
 
-    /// Track an error using an error @message and optional @params.
+    /// Track an error using an error [message] and optional [params].
     Future trackError(String message, [Map<String, dynamic>? params]);
 
-    /// Track a warning specifying its @source, with exception @e and @stackTrace.
+    /// Track a warning specifying its [source], with exception [e] and [stackTrace].
     Future trackWarningWithException(String source, dynamic e, dynamic stackTrace);
 
-    /// Track an error specifying its @source, with exception @e and @stackTrace.
+    /// Track an error specifying its [source], with exception [e] and [stackTrace].
     Future trackErrorWithException(String source, dynamic e, dynamic stackTrace);
 
-    /// Track an action event.
-    /// @param name The name of the event.
-    /// @param action The name of the action.
+    /// Track an action event with [name] and [action].
     void trackAction(String name, String action);
 
-    /// Track a value event.
-    /// @param name The name of the event.
-    /// @param value The name of the value.
+    /// Track a value event with [name] and [value].
     void trackValue(String name, Object value);
 
-    /// Track an action-and-value event.
-    /// @param name The name of the event.
-    /// @param action The name of the action.
-    /// @param value The name of the value.
+    /// Track an action-and-value event with [name] and [action] and [value].
     void trackActionAndValue(String name, String action, Object value);
 }
