90b47041c672e03a80755810e1b575f382611616
eggnstone <eggnstone@users.noreply.github.com>
2025-02-26 12:15:32 +0100

    v4.2.0: Using freezed 3.0.0 now.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 4.2.0
+
+* Using freezed 3.0.0 now.
+
 ## 4.1.2
 
 * Added (Nullable)Date to SharedPreferencesService.
--- test/Dummy_test.dart
+++ test/Dummy_test.dart
@@ -3,7 +3,8 @@ import 'package:flutter_test/flutter_test.dart';
 void main()
 {
     test('Dummy', ()
-    async
-    {
-    });
+        async
+        {
+        }
+    );
 }
